afd7e3c76d9f566798b50f28ebee77477ba589fb
cami <cami@cami.com>
2021-02-16 13:33:03 +0900

    회원기능

---
+++ src/a_branch/AClass.java
@@ -0,0 +1,10 @@
+package a_branch;
+
+public class AClass {
+
+	public static void main(String[] args) {
+		System.out.println("a가 만든 기능");
+
+	}
+
+}
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("공용 내용 틀입니다.");
-
+		
 	}
 
 }
